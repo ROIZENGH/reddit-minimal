de6695eb7f02219ffd66f5c7a871f344e7c8b9db
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-04-30 14:28:50 -0500

    import searchTerm to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchTermReducer from "../features/searchTerm/searchTermSlice.js";
+import postsReducer from "../features/posts/postsSlice.js";
 
 export default configureStore({
     reducer: {
+        searchTerm: searchTermReducer,
     }
 });

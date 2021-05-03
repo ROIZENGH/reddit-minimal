b1019623dd51bdca6972a9aacfea23cdeafd83a2
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-03 17:43:37 -0500

    loadPosts thunk

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadPosts = createAsyncThunk(
+    "posts/loadPosts",
+    async (subreddit)=> {
+        const data = await fetch(`https://www.reddit.com/${subreddit}.json`);
+        const json = await data.json();
+        return json.data.children;
+    }
+)
 
 const initialState = {
     posts: {
@@ -6,6 +15,8 @@ const initialState = {
         2: {username: "username 2", imageurl: "imageurl2", description:"post description 2", postId:"2"},
         3: {username: "username 3", imageurl: "", description:"post description 3", postId:"3"}
     },
+    isLoadingPosts: false,
+    hasError: false
 }
 
 const postsSlice = createSlice({
@@ -16,12 +27,28 @@ const postsSlice = createSlice({
             state.posts[action.payload.postid]=action.payload;
         }
     },
-    extraReducers: {
-
+    extraReducers: (builder) => {
+        builder
+        .addCase(loadPosts.pending, (state) => {
+            state.isLoadingPosts = true;
+            state.hasError = false;
+        })
+        .addCase(loadPosts.fulfilled, (state, action) => {
+            state.isLoadingPosts = false;
+            state.hasError = false;
+            state.posts = action.payload;
+        })
+        .addCase(loadPosts.rejected, (state, action) => {
+            state.isLoadingPosts = false;
+            state.hasError = true;
+            state.posts = [];
+        })
     }
 })
 
 export const selectPosts = (state) => state.posts.posts;
+
+export const isLoading = (state) => state.posts.isLoading;
 
 export const { addPost } = postsSlice.actions;
 export default postsSlice.reducer;

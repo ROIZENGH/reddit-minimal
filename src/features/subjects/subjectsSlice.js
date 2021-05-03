3b59da0b084022983b4075afd9f7cac586f33568
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-03 12:24:45 -0500

    add initial State hardcoded values

--- src/features/subjects/subjectsSlice.js
+++ src/features/subjects/subjectsSlice.js
@@ -2,7 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     subjects: {
-        1: {subjectid: "1", subjectName: "Subject 1"}
+        1: {subjectid: "1", subjectName: "Subject 1", subjectImageUrl: "imageurl1"},
+        2: {subjectid: "2", subjectName: "Subject 2", subjectImageUrl: "imageurl2"},
+        3: {subjectid: "3", subjectName: "Subject 3", subjectImageUrl: "imageurl3"}
     }
 }
 

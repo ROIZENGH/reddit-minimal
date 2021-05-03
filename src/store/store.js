4edb6971cb14371eea1de9f9c8dfae24b1841f7e
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-03 12:19:46 -0500

    add comments to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import searchTermReducer from "../features/searchTerm/searchTermSlice.js";
 import postsReducer from "../features/posts/postsSlice.js";
 import subjectsReducer from "../features/subjects/subjectsSlice.js";
+import commentsReducer from "../features/comments/commentsSlice.js";
 
 export default configureStore({
     reducer: {
         searchTerm: searchTermReducer,
         posts: postsReducer,
         subjects: subjectsReducer,
+        comments: commentsReducer
     }
 });

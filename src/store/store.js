be49020d5fad727c1b0e10fa145c5329c40ae485
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-03 12:19:34 -0500

    add subjects to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchTermReducer from "../features/searchTerm/searchTermSlice.js";
 import postsReducer from "../features/posts/postsSlice.js";
+import subjectsReducer from "../features/subjects/subjectsSlice.js";
 
 export default configureStore({
     reducer: {
         searchTerm: searchTermReducer,
         posts: postsReducer,
+        subjects: subjectsReducer,
     }
 });

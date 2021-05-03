13113eaa603d136e25b364aa8bc16b381aac9c49
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-03 12:24:16 -0500

    rename key postid for correct cammel case

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,9 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     posts: {
-        1: {username: "username 1", imageurl: "imageurl1", description:"post description 1", postid:"1"},
-        2: {username: "username 2", imageurl: "imageurl2", description:"post description 2", postid:"2"},
-        3: {username: "username 3", imageurl: "", description:"post description 3", postid:"3"}
+        1: {username: "username 1", imageurl: "imageurl1", description:"post description 1", postId:"1"},
+        2: {username: "username 2", imageurl: "imageurl2", description:"post description 2", postId:"2"},
+        3: {username: "username 3", imageurl: "", description:"post description 3", postId:"3"}
     },
 }
 

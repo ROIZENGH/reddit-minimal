067937f932f981b66b4d06881ed9e6220d9f4914
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-04 11:53:42 -0500

    load Posts

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,16 +1,45 @@
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Comments } from "../comments/Comments.js";
-import { selectPosts } from "./postsSlice.js";
+import { loadComments } from "../comments/commentsSlice.js";
+import { 
+    loadPosts, 
+    selectPosts, 
+    isLoading,
+    selectFilteredPosts
+} from "./postsSlice.js";
 
 export const Posts = () => {
-    const posts = useSelector(selectPosts);
+    const dispatch = useDispatch();
+    const isLoadingPosts = useSelector(isLoading);
+    const posts = useSelector(selectFilteredPosts);
+    
+    useEffect(() => {
+        dispatch(loadPosts("r/pics"))
+    }, [dispatch])
+
+    if(isLoadingPosts) {
+        return (
+            <React.Fragment>
+                <h2>Top Posts</h2>
+                <br />
+                <p>Loading Posts...</p>
+            </React.Fragment>
+
+        );
+    }
+
     return (
         <div>
-            <h2>POSTS</h2>
+            <h2>Top Posts</h2>
             {Object.values(posts).map((post)=>(
-                <div className="post-container">
-                    <h3>{post.username}</h3>
-                    <p>{post.description}</p>
+                <div key={post.data.id} className="post-container">
+                    <h3>{post.data.title}</h3>
+                    <div className="image-container">
+                        {post.data.url_overridden_by_dest && <img className="post-image" src={post.data.url_overridden_by_dest} alt=""/>}
+                    </div>
+                    <a href={`https://www.reddit.com/user/${post.data.author}`}>u/{post.data.author}</a>
+                    <button><img className="comments-icon" src="images/comments-512.png" alt="display comments" onClick={(e)=> dispatch(loadComments(post.data))}/>{post.data.num_comments}</button>
                     <Comments post={post}/>
                 </div>
             ))}

f30f3354ba016a2e4685c1bfdc5f5ce61bd64cf9
ROO <72269977+ROIZENGH@users.noreply.github.com>
2021-05-04 11:53:19 -0500

    load comments to component

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux"
-import { selectComments } from "./commentsSlice"
+import {
+    selectComments,
+    isLoading
+} from "./commentsSlice"
 
 export const Comments = ({post}) =>{
     const comments = useSelector(selectComments);
+    const isLoadingComments = useSelector(isLoading)
+
+    if(isLoadingComments){
+        <p>Loading Comments...</p>
+    }
+
     return (
         <div>
-            {Object.values(comments).map((comment)=>(post.postId===comment.postId &&
-            
-                <React.Fragment>
-                    <p>{comment.username}</p>
-                    <p>{comment.content}</p>
-                </React.Fragment>
+            {Object.values(comments).map((comment)=> ( comment.postId === post.postId &&
+                <div className="comment-container">
+                    <p className="username">{comment.data.author}</p>
+                    <p>{comment.data.body}</p>
+                </div>
             ))}
         </div>
     )
